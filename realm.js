9dcc1fdbd6a1e87a3ad4a9143b0b891a74e3bb65
Max tmvt <maxime.tm@gmail.com>
2018-04-25 17:28:42 +0200

    Add realm schema and set synced db

--- realm.js
+++ realm.js
@@ -1,5 +1,14 @@
 let Realm = require("realm");
 
+const UserSchema = {
+    name: "User",
+    properties: {
+        id: 'int',
+        email: 'string',
+        password: 'string'
+    }
+};
+
 module.exports.connect = (url, user, password, realm) => {
     return Realm.Sync.User.login(url, user, password).catch(err => {
         console.error("failed to auth to realm with", user, password);
@@ -8,12 +17,23 @@ module.exports.connect = (url, user, password, realm) => {
     }).then(user => {
         console.log("realm user connected");
         return Realm.open({
-            user: user,
-            url: realm
+            schema: [UserSchema],
+            sync: {
+                user: user,
+                url: realm,
+                error: err => { throw (err); }
+            }
+        }).catch(err => {
+            console.error("failed to connect to realm");
+            console.error("==> ERROR :", err);
+            user.logout();
+            process.exit(1);
         });
-    }).catch(err => {
-        console.error("failed to connect to realm");
-        console.error("==> ERROR :", err);
-        process.exit(1);
     });
 };
+
+module.exports.disconnect = () => {
+    if (Realm.Sync.User.current) {
+        Realm.Sync.User.current.logout();
+    }
+}
