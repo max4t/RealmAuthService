bb1acfd5c1d3b70785ee08590e7bbb1e6d09620b
Max tmvt <maxime.tm@gmail.com>
2018-04-25 17:30:10 +0200

    fix realm path and route registration

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ realm.connect(
     `http://${config.realmHost}:${config.realmPort}`,
     config.realmUser,
     config.realmPassword,
-    `realm://${config.realmPath}`
+    `realm://${config.realmHost}:${config.realmPort}${config.realmPath}`
 ).then(db => {
     console.log("connected to realm");
 
@@ -20,11 +20,17 @@ realm.connect(
     server.use(restify.plugins.bodyParser());
     
     // register routes from ./route.js
-    for (let routeParams of routes) {
-        server[routeParams.method](routeParams.path, routeParams.handler(db));
+    for (const [_, params] of Object.entries(routes)) {
+        server[params.method](params.path, params.handler(db));
+        console.log("Registered", params.method.toUpperCase(), params.path);
     }
     
     server.listen(8080, () => {
         console.log("server listening on 8080");
     });
+}).catch(err => {
+    realm.disconnect();
+    console.error("exited unexpectedly");
+    console.error("==> ERROR :", err);
+    process.exit(1);
 });
