f151832ac0b2e0f669b6ba0a7aa8c7f09fb447e4
Max tmvt <maxime.tm@gmail.com>
2018-04-25 02:07:33 +0200

    extract route logic

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let config = require("./config");
 config();
 
+let routes = require("./route");
+
 let restify = require('restify');
-let bcrypt = require('bcrypt');
 let Realm = require('realm');
 
 class InvalidUserError extends Error {
@@ -21,37 +22,26 @@ Realm.Sync.User.login(
     `http://${config.realmHost}:${config.realmPort}`,
     config.realmUser,
     config.realmPassword
-).then(user => {
+).catch(err => {
+    console.error("failed to auth to realm with", config.realmUser, config.realmPassword);
+    console.error("==> ERROR :", err);
+    process.exit(1);
+}).then(user => {
     console.log("realm user connect");
     return Realm.open({
         user: user,
         url: `realm://${config.realmPath}`
     });
-}).catch(err => {
-    console.error("failed to auth to realm with", config.realmUser, config.realmPassword);
-    console.error("==> ERROR :", err);
-    process.exit(1);
 }).then(realm => {
     console.log("connected to realm");
 
     let server = restify.createServer();
     server.use(restify.plugins.bodyParser());
     
-    server.post("/auth", (req, res, next) => {
-        let email = req.body.email;
-        let password = req.body.password;
-        let users = realm.objects('User').filtered(`email = "${email}"`);
-        if (users.length != 1) {
-            return next(new InvalidUserError());
-        }
-        bcrypt.compare(password, users[0].password).then(isMatch => {
-            if (!isMatch) {
-                return next(new InvalidUserError());
-            }
-            res.send(200, {id: users[0].id});
-        });
-    });
-
+    // register routes from ./route.js
+    for (let routeParams of routes) {
+        server[routeParams.method](routeParams.path, routeParams.handler);
+    }
     
     server.listen(8080, () => {
         console.log("server listening on 8080");
---
+++ route.js
@@ -0,0 +1,26 @@
+let bcrypt = require("bcrypt");
+
+function autHandler(realm)  {
+    return (req, res, next) => {
+        let email = req.body.email;
+        let password = req.body.password;
+        let users = realm.objects('User').filtered(`email = "${email}"`);
+        if (users.length != 1) {
+            return next(new InvalidUserError());
+        }
+        bcrypt.compare(password, users[0].password).then(isMatch => {
+            if (!isMatch) {
+                return next(new InvalidUserError());
+            }
+            res.send(200, {id: users[0].id});
+        });
+    }
+}
+
+module.exports = {
+    authRoute: {
+        method: "post",
+        path: "/auth",
+        handler: authHandler
+    }
+};
