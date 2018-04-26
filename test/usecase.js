f307362364b86cf92e59f6415c4a4f631bc4d9d0
Max tmvt <maxime.tm@gmail.com>
2018-04-26 14:42:52 +0200

    await in promise

--- test/usecase.js
+++ test/usecase.js
@@ -1,34 +1,41 @@
-let usecase = require("./usecase");
+let usecase = require("../usecase");
 
 let mockDb = {
     findByEmail: async (email) => {
         if (email == "test@example") {
             return [
-                {id: 5, email: "test@example", password: ""} // password = truc
+                {id: 5, email: "test@example", password: "$2a$08$1.kG00FZzg3NDvebV300A.NkVrmnTJHn.UlbzVVNfvWDHkvJMjKKa"} // password = truc
             ]
         }
         return [];
     }
 }
 
-let core = await usecase({log: () => {}}, mockDb);
+let testFct = async () => {
+    let core = await usecase({log: () => {}}, mockDb);
 
-console.log("testing unknown email");
-let id = await core.login("tt", "efsf");
-if (id !== false) {
-    console.log("fail");
-    process.exit(1);
+    console.log("testing unknown email");
+    let id = await core.login("tt", "efsf");
+    if (id !== false) {
+        console.log(" --> fail");
+        process.exit(1);
+    }
+
+    console.log("testing mismatched password");
+    id = await core.login("test@example", "efsf");
+    if (id !== false) {
+        console.log(" --> fail");
+        process.exit(1);
+    }
+    console.log("testing valid user");
+    id = await core.login("test@example", "truc");
+    if (id !== 5) {
+        console.log(" --> fail");
+        process.exit(1);
+    }
 }
 
-console.log("testing mismatched password");
-let id = await core.login("test@example", "efsf");
-if (id !== false) {
-    console.log("fail");
+testFct().catch(err => {
+    console.log("error", err);
     process.exit(1);
-}
-console.log("testing valid user");
-let id = await core.login("test@example", "truc");
-if (id !== 5) {
-    console.log("fail");
-    process.exit(1);
-}
+});
